87a92cca954c986f63ac0fc7a9c69744c578692c
loxt <emannuelloxt@hotmail.com>
2023-05-27 14:09:38 -0400

    feat: add button to navigate to home page

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:coffe_shop_app/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -19,6 +20,51 @@ class WelcomePage extends StatelessWidget {
             Text(
               "Coffe Shop",
               style: GoogleFonts.pacifico(fontSize: 50, color: Colors.white),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Feeling Low? Take a Sip of Coffee",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1,
+                      color: Colors.white.withOpacity(0.8)),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                InkWell(
+                  splashColor: Colors.black,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        ));
+                  },
+                  child: Ink(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 30),
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFE57734),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: const Text(
+                        "Get Started",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
             )
           ],
         ),
